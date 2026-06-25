c44936db60424995c94274c8d4a1180275c46277
anuj0456 <anuj0456@gmail.com>
2026-06-25 08:29:56 +0530

    Gpt-oss implemented

---
+++ gpt-oss-20B/config.py
@@ -0,0 +1,12 @@
+config = {
+    "vocab_size" : 200000,
+    "embedding_dim": 2880,
+    "context_len": 128000,
+    "num_heads": 64,
+    "num_attention_blocks": 24,
+    "hidden_dim": 2880,
+    "num_experts": 32,
+    "top_k": 4,
+    "num_kv_groups": 8,
+    "head_dim": 64,
+}
